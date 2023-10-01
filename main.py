d0d29bce46f13affc7e445e8872e2bfe3fe4286a
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2023-10-01 01:59:50 -0500

    Updated main
    Updated main.py with more comments and resolved variable names

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def processGcode(fileName, scale, offsetX, offsetY):
     # read data from the gcode file
     content = open(fileName,'r').readlines()
     i = 0 #initialise the iterator
-    d,r = [],[]
+    data,raw = [],[] #initialise the lists
     startingPoint = ';LAYER:0' # find the starting point (layer 0)
     endPoint = ';LAYER:1' # find the end point, where the next layer would start in 3D space
     for line in content:
@@ -11,24 +11,24 @@ def processGcode(fileName, scale, offsetX, offsetY):
             lnCount = i # set the count for the number of lines in layer 0 to i
             break # break for loop
         elif line.find(startingPoint) >= 0:s = i # if on layer 0 with no value, then this must be the starting point
-    for i in range(s+1,lnCount):r.append(content[i].split())
-    for i in range(0,len(r)): # iterate through the list
-        currData = r[i] # pull in the current list of data (G0, x, y)
+    for i in range(s+1,lnCount):raw.append(content[i].split())
+    for i in range(0,len(raw)): # iterate through the list
+        currData = raw[i] # pull in the current list of data (G0, x, y)
         if currData[0] == 'G1': extrude = True # if the value is G1, then move and output pixels
         elif currData[0] == 'G0': extrude = False # if the value is G0, then move but don't output picels
         else: continue # if there's no data, then continue
         x,y = currData[1],currData[2] # set the x and y coordinates from the current data set
-        if x[0] == 'X':x = currData[1]
-        elif y[0] == 'X':x,y = currData[2],currData[3]
+        if x[0] == 'X':x = currData[1] #process data if an X value into the X list
+        elif y[0] == 'X':x,y = currData[2],currData[3] # process data
         else: continue
-        d += [[((float(x[1:]) * scale) + offsetX),((float(y[1:]) * scale) + offsetY),extrude]]
-    if len(d) < 15: print(d)
-    return d
+        data += [[((float(x[1:]) * scale) + offsetX),((float(y[1:]) * scale) + offsetY),extrude]]
+    if len(data) < 15: print(data)
+    return data
 def gCode(points):
     import turtle
     wn,t = turtle.Screen(),turtle.Turtle() # initialise the turtle and turtle graphics
     wn.tracer(0)
-    for x, y, extrude in points:
+    for x, y, extrude in points: #iterate over the list, use processed data to draw with turtle
         if extrude:t.down()
         else:t.up()
         t.goto(x, y)
